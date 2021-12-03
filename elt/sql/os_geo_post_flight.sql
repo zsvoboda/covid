6a5e24e441b0e971948049ff4566aa8e77f40667
Zdenek Svoboda <zsvoboda@gmail.com>
2021-12-03 16:26:47 +0100

    Covid views

---
+++ elt/sql/os_geo_post_flight.sql
@@ -0,0 +1,9 @@
+alter table os_county add constraint os_county_country_id_fk foreign key (country_id) references os_country(country_id);
+alter table os_district add constraint os_district_county_id_fk foreign key (county_id) references os_county(county_id);
+alter table os_city add constraint os_city_district_id_fk foreign key (district_id) references os_district(district_id);
+alter table os_demography add constraint os_demography_city_id_fk foreign key (city_id) references os_city(city_id);
+
+create index os_county_country_id_idx on os_county(country_id);
+create index os_district_county_id_idx on os_district(county_id);
+create index os_city_district_id_idx on os_city(district_id);
+create index os_demography_city_id_idx on os_demography(city_id);
